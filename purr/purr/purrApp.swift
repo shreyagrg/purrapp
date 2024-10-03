8d40a166c5e0050ab3523c50626427c1e4b42925
Shreya Gurung <20163541@student.westernsydney.edu.au>
2024-10-04 09:40:30 +1000

    initialised swift project

---
+++ purr/purr/purrApp.swift
@@ -0,0 +1,17 @@
+//
+//  purrApp.swift
+//  purr
+//
+//  Created by Shreya Gurung on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct purrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
