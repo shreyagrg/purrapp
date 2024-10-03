90422fe898aad36dd353db5955be04d08b01f6ac
Shreya Gurung <20163541@student.westernsydney.edu.au>
2024-10-04 09:53:34 +1000

    Initialise app

---
+++ purr/purrTests/purrTests.swift
@@ -0,0 +1,17 @@
+//
+//  purrTests.swift
+//  purrTests
+//
+//  Created by Shreya Gurung on 04/10/2024.
+//
+
+import Testing
+@testable import purr
+
+struct purrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
