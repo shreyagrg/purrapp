b5cb51d859ae7ca7150b4d51c3235a4352434baf
Shreya Gurung <20163541@student.westernsydney.edu.au>
2024-10-04 09:45:27 +1000

    Initial change

---
+++ purr/purr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  purr
+//
+//  Created by Shreya Gurung on 04/10/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
